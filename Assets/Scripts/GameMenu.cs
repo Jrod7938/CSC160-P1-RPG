b7a5f16006a3760195a7b4eed7b71c18c13e47c7
Jancarlos Rodriguez <jrod7938@gmail.com>
2023-10-18 10:52:39 -0400

    setup stat references

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour {
 
     public GameObject theMenu;
+
+    private CharStats[] playerStats;
+    public Text[]
+        nameText,
+        hpText,
+        mpText,
+        lvlText,
+        expText;
+
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
 
     // Start is called before the first frame update
     void Start() {
@@ -20,8 +33,22 @@ public class GameMenu : MonoBehaviour {
                 GameManager.instance.gameMenuOpen = false;
             } else {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
 
+            }
+        }
+    }
+
+    public void UpdateMainStats() {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; i < playerStats.Length; i++) {
+            if (playerStats[i].gameObject.activeInHierarchy) {
+                charStatHolder[i].SetActive(true);
+
+            } else {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
